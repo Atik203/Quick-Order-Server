54f73ada75c4fffe3acee8868db603ae84fe4f33
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-25 13:19:32 +0600

    added create product routes

--- src/app/modules/product/product.routes.ts
+++ src/app/modules/product/product.routes.ts
@@ -1,9 +1,10 @@
-import { Router } from 'express';
-import { productsController } from './product.controller';
+import { Router } from "express";
+import { productsController } from "./product.controller";
 
 const router = Router();
 
-router.get('/', productsController.getAllProductsHandler);
-router.get('/:id', productsController.getProductByIdHandler);
+router.get("/", productsController.getAllProductsHandler);
+router.get("/:id", productsController.getProductByIdHandler);
+router.post("/create-product", productsController.createProductHandler);
 
 export const productRoutes = router;
